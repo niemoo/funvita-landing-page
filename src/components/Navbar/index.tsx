063c8e4caf4911b84d76cbf02c9b30fcb1dda726
Izzan Abdul <izzanabdul123@gmail.com>
2024-03-10 22:19:15 +0700

    feat: update navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import funvitaLogoBlue from '../../../public/funvita-logo-blue.png';
+import { TiThMenu } from 'react-icons/ti';
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
 
 export default function Navbar() {
   return (
@@ -22,6 +24,30 @@ export default function Navbar() {
             Kontak Kami
           </Link>
         </li>
+        <Sheet>
+          <SheetTrigger className="md:hidden">
+            <TiThMenu className="text-xl" />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle className="mb-5">Menu</SheetTitle>
+              <SheetDescription className="flex flex-col gap-2">
+                <Link href="/" className="py-2 px-3 rounded-lg hover:bg-blue-800 hover:text-white transition-all duration-300">
+                  Beranda
+                </Link>
+                <Link href="/layanan" className="py-2 px-3 rounded-lg hover:bg-blue-800 hover:text-white transition-all duration-300">
+                  Layanan
+                </Link>
+                <Link href="/about" className="py-2 px-3 rounded-lg hover:bg-blue-800 hover:text-white transition-all duration-300">
+                  Tentang Kami
+                </Link>
+                <Link href="/contact" className="py-2 px-3 rounded-lg hover:bg-blue-800 hover:text-white transition-all duration-300">
+                  Kontak Kami
+                </Link>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </div>
     </nav>
   );
